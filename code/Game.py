e591dc3f89d954e40800e3de752464b53a76b0bc
Marcos <marcosantonio240604@gmail.com>
2026-04-15 10:49:49 -0300

    Creating asset folder Inserting all assets Creating the menu Creating the const file

---
+++ code/Game.py
@@ -0,0 +1,19 @@
+import pygame
+
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(576, 324))
+
+    def run(self, ):
+
+
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
